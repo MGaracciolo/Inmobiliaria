7daebfe8ea7db040aff978fc6afcb0ae4c823fb7
Gabriel Melian <125325749+Gabriel-Melian@users.noreply.github.com>
2024-08-29 16:22:52 -0300

    Se mejora verificacion again

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -7,14 +7,16 @@ public class Inquilino
 
     [Required(ErrorMessage = "El nombre es obligatorio.")]//Melian
     [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
+    [MinLength(2, ErrorMessage = "El nombre debe tener al menos 2 caracteres.")]
     public string? nombre { get; set; }
 
     [Required(ErrorMessage = "El apellido es obligatorio.")]
     [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El apellido solo puede contener letras y espacios.")]
+    [MinLength(2, ErrorMessage = "El apellido debe tener al menos 2 caracteres.")]
     public string? apellido { get; set; }
 
     [Required(ErrorMessage = "El DNI es obligatorio.")]
-    [RegularExpression(@"\d{7,8}", ErrorMessage = "El DNI debe tener entre 7 y 8 digitos y no debe contener letras.")]
+    [RegularExpression(@"^\d{7,8}[A-Z]?$", ErrorMessage = "El DNI debe tener entre 7 y 8 digitos, y puede incluir una letra al final.")]
     public string? dni { get; set; }
 
     [Required(ErrorMessage = "El email es obligatorio.")]
