885e466ba7c2d5968b86695529f7ef0c16eb4363
Gabriel Melian <125325749+Gabriel-Melian@users.noreply.github.com>
2024-08-29 15:53:15 -0300

    Se mejora la verificacion

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -6,21 +6,23 @@ public class Inquilino
     public int id_inquilino { get; set; }
 
     [Required(ErrorMessage = "El nombre es obligatorio.")]//Melian
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string? nombre { get; set; }
 
     [Required(ErrorMessage = "El apellido es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El apellido solo puede contener letras y espacios.")]
     public string? apellido { get; set; }
 
     [Required(ErrorMessage = "El DNI es obligatorio.")]
-    [RegularExpression(@"\d{7,8}", ErrorMessage = "El DNI debe tener entre 7 y 8 dígitos.")]
+    [RegularExpression(@"\d{7,8}", ErrorMessage = "El DNI debe tener entre 7 y 8 digitos y no debe contener letras.")]
     public string? dni { get; set; }
 
     [Required(ErrorMessage = "El email es obligatorio.")]
     [EmailAddress(ErrorMessage = "El email no tiene un formato válido.")]
     public string? email { get; set; }
 
-    [Required(ErrorMessage = "El teléfono es obligatorio.")]
-    [Phone(ErrorMessage = "El teléfono no tiene un formato válido.")]
+    [Required(ErrorMessage = "El telefono es obligatorio.")]
+    [Phone(ErrorMessage = "El telefono no tiene un formato válido.")]
     public string? telefono { get; set; }
 
     public override string ToString()
